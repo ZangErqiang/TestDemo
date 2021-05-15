e9d4813b331f795b4a9d385c8e718911a0cdf115
zeq <2221005886@qq.com>
2021-05-15 22:22:09 +0800

    修改类Test01

--- src/main/java/person/zangerqiang/Test01.java
+++ src/main/java/person/zangerqiang/Test01.java
@@ -3,5 +3,6 @@ package person.zangerqiang;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("Test01");
+        System.out.println("hello github");
     }
 }
